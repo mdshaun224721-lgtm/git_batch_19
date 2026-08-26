22a287dde98e7b592f0b0286df366fa11635f422
mdshaun224721-lgtm <mdshaun224721@gmail.com>
2026-08-27 03:55:50 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
 void main(){
   List<int> hasib=[1,2,3,4,5,6,7,8,9];
   print(hasib);
+
+  
+  List <String> list1=["01845674567","01845678765","0182437876","01844567876"];
+  for(var list2 in list1){
+    print("$list2 your current balance is very low now");
+  }
+  
 }
